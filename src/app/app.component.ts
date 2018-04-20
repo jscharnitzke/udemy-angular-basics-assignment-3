c96e9311df9145e7e8bae01434418d8f4a3a19be
Joe Scharnitzke <j.scharnitzke@gmail.com>
2018-04-19 19:26:48 -0700

    Completed the assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  showSecret = false;
+  clicks = [];
+
+  onToggleSecret() {
+    this.showSecret = !this.showSecret;
+    this.clicks.push(Date.now());
+  }
+
+  isAfterFour(click: Date) {
+    return this.clicks.indexOf(click) > 4;
+  }
+
+  getBackgroundColor(click: Date) {
+    return this.isAfterFour(click) ? 'blue' : 'white';
+  }
 }
